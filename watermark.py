19056d1983b81d8d88683abbcaa53be73f8ae619
Jason <jasonliu215@gmail.com>
2022-07-14 14:27:19 +1200

    New logo for watermarking videos

--- watermark.py
+++ watermark.py
@@ -1,6 +1,6 @@
 import subprocess
 
-command_line = f'ffmpeg -i test.mp4 -i logo.gif -filter_complex "overlay=W-w-5:H-h-5" -c:a copy watermarked_video.mp4'
+command_line = f'ffmpeg -i timelapse_video.mp4 -i logo.gif -filter_complex "overlay=W-w-5:H-h-5" -c:a copy watermarked_video.mp4'
 
 # command_line = f"ffmpeg -i timelapse_video.mp4 -i logo.png -filter_complex \
 # '[0:v][1:v]overlay=15:10[outv]' -map [outv] -map [0:a] \
